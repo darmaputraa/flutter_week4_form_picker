1eb6ab1ae76fd007b6ab6fc051f1c655b9a85582
darmaputraa <irvandarmaputra@gmail.com>
2023-03-05 21:50:57 +0700

    draft form picker

--- lib/create_post_page.dart
+++ lib/create_post_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:open_file/open_file.dart';
+import 'package:intl/intl.dart';
 
 class CreatePostPage extends StatefulWidget {
   const CreatePostPage({super.key});
@@ -9,20 +10,35 @@ class CreatePostPage extends StatefulWidget {
   State<CreatePostPage> createState() => _CreatePostPageState();
 }
 
-void _pickFile() async {
-  final result = await FilePicker.platform.pickFiles();
-  if (result == null) return;
-
-  // Hasil file dari object result
-  final file = result.files.first;
-  _openFile(file);
-}
-
-void _openFile(PlatformFile file) {
-  OpenFile.open(file.path);
-}
-
 class _CreatePostPageState extends State<CreatePostPage> {
+  void _pickFile() async {
+    final result = await FilePicker.platform.pickFiles();
+    if (result == null) return;
+
+    // Hasil file dari object result
+    final file = result.files.first;
+    _openFile(file);
+  }
+
+  void _openFile(PlatformFile file) {
+    OpenFile.open(file.path);
+  }
+
+  DateTime selectedDate = DateTime.now();
+
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? date = await showDatePicker(
+        context: context,
+        initialDate: selectedDate,
+        firstDate: DateTime(1980),
+        lastDate: DateTime(2028));
+    if (date != null && date != selectedDate) {
+      setState(() {
+        selectedDate = date;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,8 +49,7 @@ class _CreatePostPageState extends State<CreatePostPage> {
         body: SafeArea(
           child: Padding(
             padding: const EdgeInsets.fromLTRB(12, 20, 12, 12),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+            child: ListView(
               children: [
                 const Text(
                   'Pick Files',
@@ -52,6 +67,44 @@ class _CreatePostPageState extends State<CreatePostPage> {
                         _pickFile();
                       },
                     ))
+                  ],
+                ),
+                const SizedBox(
+                  height: 2,
+                ),
+                const Text('Tanggal Lahir',
+                    style:
+                        TextStyle(fontWeight: FontWeight.w700, fontSize: 15)),
+                const SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  children: <Widget>[
+                    Expanded(
+                        child: Container(
+                            decoration: BoxDecoration(
+                                border:
+                                    Border.all(width: 1.0, color: Colors.black),
+                                borderRadius: BorderRadius.circular(10)),
+                            child: Padding(
+                                padding: const EdgeInsets.all(8),
+                                child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    children: <Widget>[
+                                      InkWell(
+                                        child: Text(
+                                            DateFormat('dd-MM-yyyy')
+                                                .format(selectedDate),
+                                            textAlign: TextAlign.start,
+                                            style: const TextStyle(
+                                                color: Color.fromARGB(
+                                                    255, 0, 0, 0),
+                                                fontSize: 22)),
+                                        onTap: () {
+                                          _selectDate(context);
+                                        },
+                                      ),
+                                    ]))))
                   ],
                 )
               ],
